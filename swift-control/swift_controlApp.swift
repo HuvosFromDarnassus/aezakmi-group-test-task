7a393a6e88cecf339f943c19351d7b2acb29af53
Daniil Tvorun <danieltvorun@icloud.com>
2025-08-12 08:56:54 -0300

    Initial Commit

---
+++ swift-control/swift_controlApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_controlApp.swift
+//  swift-control
+//
+//  Created by Daniel Tvorun on 12/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct swift_controlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
